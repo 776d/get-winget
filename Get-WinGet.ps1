5c4532494d5aac629c4a4657da6f2b4dbc97d4aa
776d <165719044+776d@users.noreply.github.com>
2025-01-11 22:41:50 +0000

    change deps source to wingets github release, put winget license hash into a variable so it can be more easily changed, couple of other minor changes

--- Get-WinGet.ps1
+++ Get-WinGet.ps1
@@ -5,29 +5,23 @@ param(
     [switch]$Version
 )
 
+$ProgressPreference = "SilentlyContinue"
+
 $scriptName = "$($MyInvocation.MyCommand.Name)"
-$scriptVersion = "1.0.0"
+$scriptVersion = "1.1.0"
 
-$vclibsURI = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
-$vclibsFileName = "Microsoft.VCLibs.appx"
-
-$xamlVer = "2.8.6"
-$xamlVerMajMin = ($xamlVer[0..2]) -join "" # Maj.Min
-$xamlURI = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v${xamlVer}/Microsoft.UI.Xaml.${xamlVerMajMin}.x64.appx"
-$xamlFileName = "Microsoft.UI.Xaml.appx"
-
-$wingetVer = "1.8.1911"
+$wingetVer = "1.9.25200"
+$wingetLicenseHash = "7fdfd40ea2dc40deab85b69983e1d873"
+$wingetDepsURI = "https://github.com/microsoft/winget-cli/releases/download/v${wingetVer}/DesktopAppInstaller_Dependencies.zip"
 $wingetURI = "https://github.com/microsoft/winget-cli/releases/download/v${wingetVer}/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-$wingetLicenseURI = "https://github.com/microsoft/winget-cli/releases/download/v${wingetVer}/76fba573f02545629706ab99170237bc_License1.xml"
-# cannot use newest version because i cannot find the updated
-# version of vclibs. just update winget after it is installed
-#$wingetVer = "1.9.25200"
-#$wingetURI = "https://github.com/microsoft/winget-cli/releases/download/v${wingetVer}/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-#$wingetLicenseURI = "https://github.com/microsoft/winget-cli/releases/download/v${wingetVer}/7fdfd40ea2dc40deab85b69983e1d873_License1.xml"
+$wingetLicenseURI = "https://github.com/microsoft/winget-cli/releases/download/v${wingetVer}/${wingetLicenseHash}_License1.xml"
+$wingetDepsFileName = "DesktopAppInstaller_Dependencies.zip"
+$wingetDepsExtractedDir = "DesktopAppInstaller_Dependencies"
 $wingetFileName = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 $wingetLicenseFileName = "License1.xml"
 
 $downloadDir = $env:TEMP
+$currentUser = $env:USERNAME
 
 function Show-Help {
     Write-Host "${scriptName}: Install WinGet and its dependencies for User scope or Machine scope"
@@ -42,24 +36,17 @@ function Show-Version {
 }
 
 function Get-Files {
-    # we need to do this because winget takes forever to download otherwise
-    $ProgressPreference = "SilentlyContinue"
-
     $retryCounter = 0
 
     $downloadedFiles = @(
-        "$downloadDir\$vclibsFileName",
-        "$downloadDir\$xamlFileName",
+        "$downloadDir\$wingetDepsFileName",
         "$downloadDir\$wingetFileName",
         "$downloadDir\$wingetLicenseFileName"
     )
 
     while ($true) {
-        Write-Host "Downloading Microsoft VCLibs"
-        Invoke-WebRequest -Uri $vclibsURI -OutFile $downloadDir\$vclibsFileName
-
-        Write-Host "Downloading Microsoft UI Xaml"
-        Invoke-WebRequest -Uri $xamlURI -OutFile $downloadDir\$xamlFileName
+        Write-Host "Downloading WinGet Dependencies"
+        Invoke-WebRequest -Uri $wingetDepsURI -OutFile $downloadDir\$wingetDepsFileName
 
         Write-Host "Downloading WinGet"
         Invoke-WebRequest -Uri $wingetURI -OutFile $downloadDir\$wingetFileName
@@ -76,15 +63,12 @@ function Get-Files {
             Break
         }
     }
-
-    # restore progress bar
-    $ProgressPreference = "Continue"
 }
 
 function Remove-Files {
-    Remove-Item -Path `
-        $downloadDir\$vclibsFileName,
-        $downloadDir\$xamlFileName,
+    Remove-Item -Force -Recurse -Path `
+        $downloadDir\$wingetDepsFileName,
+        $downloadDir\$wingetDepsExtractedDir,
         $downloadDir\$wingetFileName,
         $downloadDir\$wingetLicenseFileName
 }
@@ -92,22 +76,23 @@ function Remove-Files {
 function Install-CurrentUser {
     Get-Files
 
-    $pkg = "$downloadDir\$vclibsFileName"
-    Write-Host "Installing Microsoft VCLibs for the current user (${env:UserName})"
+    Expand-Archive -Path $downloadDir\$wingetDepsFileName -DestinationPath $downloadDir\$wingetDepsExtractedDir
+
+    $pkg = (Resolve-Path "$downloadDir\$wingetDepsExtractedDir\x64\Microsoft.VCLibs*.appx").Path
+    Write-Host "Installing WinGet Dependencies for the current user ($currentUser)"
     Start-Job -Name AddVCLibsUserJob -ScriptBlock {
         Add-AppxPackage $using:pkg
     } | Out-Null
     Wait-Job -Name AddVCLibsUserJob | Out-Null
 
-    $pkg = "$downloadDir\$xamlFileName"
-    Write-Host "Installing Microsoft UI Xaml for the current user (${env:UserName})"
+    $pkg = (Resolve-Path "$downloadDir\$wingetDepsExtractedDir\x64\Microsoft.UI.Xaml*.appx").Path
     Start-Job -Name AddXamlUserJob -ScriptBlock {
         Add-AppxPackage $using:pkg
     } | Out-Null
     Wait-Job -Name AddXamlUserJob | Out-Null
 
     $pkg = "$downloadDir\$wingetFileName"
-    Write-Host "Installing WinGet for the current user (${env:UserName})"
+    Write-Host "Installing WinGet for the current user ($currentUser)"
     Start-Job -Name AddWinGetUserJob -ScriptBlock {
         Add-AppxPackage $using:pkg
     } | Out-Null
@@ -117,15 +102,16 @@ function Install-CurrentUser {
 function Install-Machine {
     Get-Files
 
-    $pkg = "$downloadDir\$vclibsFileName"
-    Write-Host "Installing Microsoft VCLibs for the machine"
+    Expand-Archive -Path $downloadDir\$wingetDepsFileName -DestinationPath $downloadDir\$wingetDepsExtractedDir
+
+    $pkg = (Resolve-Path "$downloadDir\$wingetDepsExtractedDir\x64\Microsoft.VCLibs*.appx").Path
+    Write-Host "Installing WinGet Dependencies for the machine"
     Start-Job -Name AddVCLibsMachineJob -ScriptBlock {
         Add-AppxProvisionedPackage -Online -PackagePath $using:pkg -SkipLicense
     } | Out-Null
     Wait-Job -Name AddVCLibsMachineJob | Out-Null
 
-    $pkg = "$downloadDir\$xamlFileName"
-    Write-Host "Installing Microsoft UI Xaml for the machine"
+    $pkg = (Resolve-Path "$downloadDir\$wingetDepsExtractedDir\x64\Microsoft.UI.Xaml*.appx").Path
     Start-Job -Name AddXamlMachineJob -ScriptBlock {
         Add-AppxProvisionedPackage -Online -PackagePath $using:pkg -SkipLicense
     } | Out-Null
@@ -150,8 +136,8 @@ function Test-WinGet {
         Exit 1
     } else {
         Write-Host "WinGet successfully installed. Please update WinGet with:"
-        Write-Host '  winget source update'
-        Write-Host '  winget update --all'
+        Write-Host "  winget source update"
+        Write-Host "  winget update --all"
     }
 }
 
@@ -176,4 +162,5 @@ if ($User -and $Machine) {
 
 Remove-Files
 Test-WinGet
+$ProgressPreference = "Continue"
 Exit
